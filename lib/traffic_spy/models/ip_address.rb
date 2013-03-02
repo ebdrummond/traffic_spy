ba290d71c21f2e6b2609ed167fa48802d7c77d2f
Kyle Suss <susskyle@gmail.com>
2013-03-02 16:32:49 -0700

    ip address

--- lib/traffic_spy/models/ip_address.rb
+++ lib/traffic_spy/models/ip_address.rb
@@ -5,5 +5,29 @@ module TrafficSpy
     def initialize(input)
       @ip_address = input
     end
+
+    def self.exists?(ip_address)
+      DB[:ip_addresses].where(:ip_address => ip_address).to_a.count > 0
+    end
+
+    def self.get_id(ip_address)
+      ip_address_row = DB[:ip_addresses].where(:ip_address => ip_address).to_a
+      ip_address_id = ip_address_row[0][:id]
+    end
+
+    def self.make_new_object(ip_address)
+      if exists?(ip_address)
+        get_id(ip_address)
+      else
+        ip_address_instance = IpAddress.new(ip_address)
+        ip_address_instance.register
+        get_id(ip_address)
+      end
+    end
+
+    def register
+      DB[:ip_addresses].insert(:ip_address => @ip_address)
+      return true
+    end
   end
 end
--- spec/traffic_spy/models/ip_address_spec.rb
+++ spec/traffic_spy/models/ip_address_spec.rb
@@ -2,6 +2,13 @@ require 'spec_helper'
 
 module TrafficSpy
   describe IpAddress do
+
+    before(:each) do
+      @ip_address = "1.2.3.4.5"
+      @ip_address_instance = Event.new(@ip_address)
+      @ip_address_instance.register
+    end
+
     it "exists" do
       IpAddress.should be
     end
@@ -9,6 +16,31 @@ module TrafficSpy
     it "should take in URL string" do
       instance = IpAddress.new("63.29.38.211")
       expect(instance.ip_address).to eq "63.29.38.211"
+    end
+
+    it "tests whether a ip_address already exists" do
+      expect(IpAddress.exists?(@ip_address)).to be true
+    end
+
+    it "should take in ip_address string" do
+      instance = IpAddress.new("socialLogin")
+      expect(instance.ip_address).to eq "socialLogin"
+    end
+
+    it "registers a new ip_address in the ip_addresss table" do
+      expect(@ip_address_instance.register).to eq true
+    end
+
+    it "returns the id of the row in the table" do
+      expect(IpAddress.get_id(@ip_address)).to be_kind_of(Integer)
+    end
+
+    it "makes a new object with a value that already exists" do
+      expect(IpAddress.make_new_object(@ip_address)).to be_kind_of(Integer)
+    end
+
+    it "makes a new object with a new ip_address" do
+      expect(IpAddress.make_new_object("2.5.6.7.8")).to be_kind_of(Integer)
     end
   end
 end
