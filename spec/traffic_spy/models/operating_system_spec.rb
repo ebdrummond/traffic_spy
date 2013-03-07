f7ec51578f74c8f78ac8f5cfa66eac1f613ed84e
Kyle Suss <susskyle@gmail.com>
2013-03-06 17:01:37 -0700

    oss update

--- spec/traffic_spy/models/operating_system_spec.rb
+++ spec/traffic_spy/models/operating_system_spec.rb
@@ -52,7 +52,8 @@ module TrafficSpy
           "eventName" => "socialLogin",
           "userAgent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2)" +
             " AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0" +
-            " Safari/537.17",          "resolutionWidth" => "1920",
+            " Safari/537.17",          
+          "resolutionWidth" => "1920",
           "resolutionHeight" => "1280",
           "ip" => "63.29.38.211"
         }.to_json)
@@ -63,7 +64,7 @@ module TrafficSpy
 
     it "returns operating systems breakdown" do
       operating_system.register
-      expect(OperatingSystem.breakdown("jean-luc")).to eq({"Mac OS X"=>1})
+      expect(OperatingSystem.breakdown("jean-luc")).to eq({"Macintosh"=>1})
     end
   end
 end
